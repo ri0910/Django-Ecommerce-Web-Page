4a555bfd2dda7902121181af9554276530fc2f23
Riya karmakar <riyakarmakar.kolkata@gmail.com>
2024-01-07 20:12:24 +0530

    Added few more new functionallities

--- Django Ecommerce Website/store/views.py
+++ Django Ecommerce Website/store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from store.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -13,11 +16,32 @@ def store(request, category_slug=None):
         products = Product.objects.filter(
             category=categories, is_available=True)
         product_count = products.count()
+        paginator = Paginator(products, 2)
+        page = request.GET.get('page')
+        paginated_page = paginator.get_page(page)
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         product_count = products.count()
+        paginator = Paginator(products, 5)
+        page = request.GET.get('page')
+        paginated_page = paginator.get_page(page)
     context = {
-        'products': products,
+        'products': paginated_page,
         'product_count': product_count
     }
     return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(
+            category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product).exists()
+    except Exception as e:
+        raise e
+    context = {
+        'single_product': single_product,
+        'in_cart': in_cart
+    }
+    return render(request, 'store/product_detail.html', context)
